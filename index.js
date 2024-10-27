b78616b2e46a2b87420071e216db2892d330360f
Pravalika2030 <varathiyapravalikanaik15@gmail.com>
2024-10-27 13:45:37 +0530

    commit

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ app.post('/visualize', (req, res) => {
         });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
